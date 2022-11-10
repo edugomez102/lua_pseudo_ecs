6f715dd0f1cb155d688ff6b4e4a9276f1786fcd6
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2022-11-10 21:04:06 +0000

    clean entity man

--- engine/man/entity_man.lua
+++ engine/man/entity_man.lua
@@ -7,30 +7,18 @@ local EM = {
   storage = {},
 }
 
--- local function generate_cmps(p_cmp_list)
---   local cmps = {}
---   for key, value in pairs(p_cmp_list) do
---     cmps[key] = CMP[key](value)
---   end
---   return cmps
--- end
-
 function EM:create_entity(p_e)
   self.storage[#self.storage + 1] = p_e
   return p_e
 end
 
 --
--- Creates entities of given list index of EM.lists
+-- Creates entities of given list
 -- 
 --@param e_list_index table with list of entities
 function EM:init_from_list(p_entity_list)
   for i = 1, #p_entity_list do
     self:create_entity(e.new(p_entity_list[i]))
-
-    -- TODO improve
-    -- local ent = self:create_entity(e.new({}))
-    -- ent.cmps = generate_cmps(self.lists[e_list_index][i].cmps)
 
   end
 end
