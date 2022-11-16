2c4953293d8594c77e62264333be3754f9f06bbe
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2022-11-16 01:52:02 +0000

    [master]: update SM to have named systems and follow and update order

--- engine/man/sys_man.lua
+++ engine/man/sys_man.lua
@@ -3,31 +3,50 @@
 -- System of ECS entities.
 
 ---
+--- System names and order in which they will update
+---
+---@table sys_index
+local sys_index = {
+  "render",
+  "physics",
+  "ai",
+  "input",
+  "collision",
+}
+
+---
+--- Load systems from file using sys_index as
+--- system order
+---
+---@return table const table containing systems 
+local function load_sys()
+  local sys = {}
+  for i = 1, #sys_index do
+    sys[sys_index[i]] = require("engine.sys." .. sys_index[i])
+  end
+  return table.protect(sys)
+end
+
+---
 --- System Manager TODO
 ---
 local SM = {
-  systems = {
-    require("engine.sys.render"),
-    require("engine.sys.physics"),
-    require("engine.sys.ai"),
-    require("engine.sys.input"),
-    require("engine.sys.collision"),
-  }
+  systems = load_sys() -- TODO more clear
 }
 
 ---
 ---@param Game any
 function SM:init(Game)
-  for i = 1, #self.systems do
-    self.systems[i]:init(Game)
+  for i = 1, #sys_index do
+    self.systems[sys_index[i]]:init(Game)
   end
 end
 
 ---
 ---@param storage any
 function SM:update(storage)
-  for i = 1, #self.systems do
-    self.systems[i].update(storage)
+  for i = 1, #sys_index do
+    self.systems[sys_index[i]].update(storage)
   end
 end
 
