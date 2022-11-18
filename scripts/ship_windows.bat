3f7874462e480b5d0a701bd57419dc4d23bb83ec
David Martinez <davidmg103@hotmail.com>
2022-11-18 18:15:05 +0100

    [master]: Gitignore and script modified

--- scripts/ship_windows.bat
+++ scripts/ship_windows.bat
@@ -2,7 +2,7 @@
 cd ..
 
 @REM Removing past builds if that is the case 
-rd /s /q Game-Win64
+rd /s /q shipping/game_win64
 
 @REM Making the .love file
 7z a -tzip Game.zip game engine modules main.lua
@@ -15,8 +15,9 @@ copy /b "C:\Program Files\LOVE\love.exe" + Game.love Game.exe
 del Game.love
 
 @REM Making the game folder ready for shipping
-mkdir Game-Win64
-move Game.exe Game-Win64\Game.exe
-copy /v /b "C:\Program Files\LOVE\*.dll" Game-Win64
+mkdir shipping
+mkdir shipping\game_win64
+move Game.exe shipping\game_win64\Game.exe
+copy /v /b "C:\Program Files\LOVE\*.dll" shipping\game_win64
 
 PAUSE
