638b3069f6565d1f1cc0927397924529925b4c5e
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2022-11-23 17:36:45 +0000

    [editor]: update Game to use and load scenes from file reading dir with system ls command

--- game/game.lua
+++ game/game.lua
@@ -5,6 +5,22 @@
 
 require("game.entity_types")
 
+-- TODO check OS
+local function load_scenes()
+  local scene_table = {}
+  local handle = io.popen("ls game/resources/scenes")
+  if handle then
+    local result = handle:read("a")
+    for line in string.lines(result) do
+      local no_extension = line:sub(0, -5)
+      scene_table[no_extension] =
+      require("game.resources.scenes." .. no_extension)
+    end
+    handle:close()
+    return scene_table
+  end
+end
+
 --- TODO
 ---@table Game
 local Game = {
@@ -18,11 +34,17 @@ local Game = {
     ai  = require("game.beh.ai_beh"),
     col = require("game.beh.col_beh")
   },
+  current_scene = nil,
+  scenes = load_scenes(),
   levels = {
     level_test = require("game.resources.e_list.entity_list_test"),
     level_01   = {}
   }
 }
+
+function Game:reload_scenes()
+  self.scenes = load_scenes()
+end
 
 --- Adds systems of the game to the engine SM
 function Game:add_game_sys(SM)
@@ -33,6 +55,13 @@ function Game:add_game_cmp(EM)
 end
 
 function Game:init(EM, SM)
+end
+
+---@param EM table
+---@param p_scene string
+function Game:load_scene(EM, p_scene)
+  EM:init_from_list(table.deepcopy(self.scenes[p_scene].entities))
+  self.current_scene = p_scene
 end
 
 ---
