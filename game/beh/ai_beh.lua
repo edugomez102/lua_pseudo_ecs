adbf5d0efc59e33395fee9dbb9685766d2a97fb3
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2022-11-23 17:22:52 +0000

    [editor]: update ai_beh to use cmp aim field

--- game/beh/ai_beh.lua
+++ game/beh/ai_beh.lua
@@ -40,15 +40,15 @@ function ai_beh.patrol_move(p_e)
   local cmp_tra = p_e.cmps.transform
   local cmp_ai  = p_e.cmps.ai
 
+  -- TODO improve
   local patrol = require("game.resources.patrol_data")[cmp_ai.patrol]
 
-  local to_x = patrol[cmp_ai.step][1]
-  local to_y = patrol[cmp_ai.step][2]
+  cmp_ai.aim.x = patrol[cmp_ai.step][1]
+  cmp_ai.aim.y = patrol[cmp_ai.step][2]
 
-  ai_utils.seek_to(p_e, to_x, to_y)
+  ai_utils.seek_to(p_e, cmp_ai.aim.x, cmp_ai.aim.y)
 
-  -- if cmp_phy.vx == 0 and cmp_phy.vy == 0 then
-  if cmp_tra.pos.x == to_x and cmp_tra.pos.y == to_y then
+  if cmp_tra.pos.x == cmp_ai.aim.x and cmp_tra.pos.y == cmp_ai.aim.y then
     if cmp_ai.step == #patrol then
       cmp_ai.step = 1
     else
