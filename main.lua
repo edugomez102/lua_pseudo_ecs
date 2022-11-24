55035274b6865d7394c98fd0748a96936505d15a
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2022-11-24 15:15:26 +0000

    [editor]: update main to game use scene instead of level

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@
 -- Inits EM, SM and game.
 -- Updates EM and SM.
 require("modules.table_")
+require("modules.string_")
+require("engine.log")
 
 local Game   = require("game.game")
 local EM     = require("engine.man.entity_man")
@@ -14,9 +16,14 @@ local love_callbacks = require("engine.love_callbacks")
 --- Inits Game and engine managers
 function love.load()
   SM:init(Game, Editor)
-  Game:load_level(EM, "level_test")
+  Editor.init(EM, SM, Game)
 
-  Editor.init(EM, SM)
+  -- TODO default scene
+  -- local default_scn = "quetal"
+  -- Game:load_scene(EM, "quetal")
+  -- local g_info  = Game.scenes[default_scn].editor_info
+  -- Editor.info = g_info
+
 end
 
 -- TODO variable game framerate
