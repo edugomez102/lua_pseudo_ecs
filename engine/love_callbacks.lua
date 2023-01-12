60feaf7e20526efc392b9279cbfc684bb1e03819
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2023-01-12 16:45:25 +0100

    [editor]: update love_callbacks

--- engine/love_callbacks.lua
+++ engine/love_callbacks.lua
@@ -1,59 +1,65 @@
 ---
 --- Runs callbacks of love like love.quit, love.mousemoved etc
 ---
----@param Editor table
-return function(Editor)
 
-  function love.quit()
-    Editor.imgui.ShutDown();
-  end
+------------------------------------------------------- DEBUG
+if OG_DEBUG then
+  return function(input, Editor)
 
-  function love.textinput(t)
+    function love.quit()
+      Editor.imgui.ShutDown();
+    end
+
+    function love.textinput(t)
       Editor.imgui.TextInput(t)
       -- if not Editor.imgui.GetWantCaptureKeyboard() then
       --     -- Pass event to the game
       -- end
-  end
+    end
 
-  function love.keypressed(key)
+    function love.keypressed(key)
       Editor.imgui.KeyPressed(key)
       -- if not Editor.imgui.GetWantCaptureKeyboard() then
       --     -- Pass event to the game
       -- end
-  end
+    end
 
-  function love.keyreleased(key)
+    function love.keyreleased(key)
       Editor.imgui.KeyReleased(key)
       -- if not Editor.imgui.GetWantCaptureKeyboard() then
       --     -- Pass event to the game
       -- end
-  end
+    end
 
-  function love.mousemoved(x, y)
+    function love.mousemoved(x, y)
       Editor.imgui.MouseMoved(x, y)
       -- if not Editor.imgui.GetWantCaptureMouse() then
       --     -- Pass event to the game
       -- end
-  end
+    end
 
-  function love.mousepressed(x, y, button)
+    function love.mousepressed(x, y, button)
       Editor.imgui.MousePressed(button)
       -- if not Editor.imgui.GetWantCaptureMouse() then
       --     -- Pass event to the game
       -- end
-  end
+    end
 
-  function love.mousereleased(x, y, button)
+    function love.mousereleased(x, y, button)
       Editor.imgui.MouseReleased(button)
       -- if not Editor.imgui.GetWantCaptureMouse() then
       --     -- Pass event to the game
       -- end
-  end
+    end
 
-  function love.wheelmoved(x, y)
+    function love.wheelmoved(x, y)
       Editor.imgui.WheelMoved(y)
       -- if not Editor.imgui.GetWantCaptureMouse() then
       --     -- Pass event to the game
       -- end
+    end
+  end
+else
+  return function(input)
   end
 end
